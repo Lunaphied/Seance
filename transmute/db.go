6a4451c3184987457377496db6c3e4d5698a7458
Lunaphied <lunaphied@lunaphied.me>
2024-04-11 17:58:59 -0600

    transmute: remove fields that can be stored on the Mattermost user

--- transmute/db.go
+++ transmute/db.go
@@ -10,9 +10,6 @@ import (
 type Collective struct {
 	gorm.Model
 
-	// System name.
-	Name string
-
 	// Mattermost user ID of base Mattermost account.
 	UserID string
 
@@ -26,14 +23,8 @@ type Collective struct {
 type Member struct {
 	gorm.Model
 
-	// Member name. (Convenience for commands, default display name).
-	Name string
-
 	// User ID for the Mattermost user that is used to proxy this member's messages.
 	UserID string
-
-	// Member display name (Nickname in Mattermost). May be empty (null).
-	DisplayName *string
 
 	// Regex representing this member's proxy pattern to match in incoming messages.
 	Regex string
@@ -44,19 +35,20 @@ type Member struct {
 	// Pronouns for this member.
 	Pronouns string
 
-	// TODO: Is this required to be stored? I think we can just keep track of it on the Mattermost side.
-	// Path to where this Member's avatar is stored (this is only used on upload).
-	// AvatarURL string
-
 	// TODO: Figure out how to chain a deletion of a Collective to deleting all it's members.
 }
 
 // TODO: wrap this in a higher level struct to shield the rest of the logic a bit.
 func (p *Plugin) openDB() (*gorm.DB, error) {
+
+	// We make our database inside the plugin's bundle path which is a writable state directory.
+	// Get the directory for the bundle.
 	plugin_dir, err := p.API.GetBundlePath()
 	if err != nil {
 		return nil, err
 	};
+
+	// Actually open the database.
 	db, err := gorm.Open(sqlite.Open(fmt.Sprintf("%s/transmute.db", plugin_dir)), &gorm.Config{})
 
 	// Errors are passed upwards.
